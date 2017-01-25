4896e0594682364052c30d701e017f8dcd2d03ce
michael_kahle <michael.kahle@ub.uni-freiburg.de>
2017-01-25 18:43:57 +0100

    Use namespace

--- example.R
+++ example.R
@@ -1,11 +1,10 @@
 # needs devtools to install via github 
-#
 ## install.packages("devtools", dependencies = TRUE)
 ## library("devtools")
-install_github('tambora-org/tamboRapi')
+devtools::install_github('tambora-org/tamboRapi')
 
 # inquire data from collection 156: The Grotzfeld data set
-data <- fromTambora("g[cid]=156")
+data <- tamboRapi::fromTambora("g[cid]=156")
 
 # draw a map of all events
-drawWorldMapAll(data)
+tamboRapi::drawWorldMapAll(data)
