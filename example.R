f06cf7e2d3bbd1b45a1598d07d075ef0767e56a2
michael_kahle <michael.kahle@ub.uni-freiburg.de>
2017-01-25 16:14:58 +0100

    Some comments in example

--- example.R
+++ example.R
@@ -1,4 +1,11 @@
+# needs devtools to install via github
+#
+## install.packages("devtools")
+## library("devtools")
 install_github('tamboRapi','tambora-org')
 
+# inquire data from collection 156: The Grotzfeld data set
 data <- fromTambora("g[cid]=156")
+
+# draw a map of all events
 drawWorldMapAll(data)
