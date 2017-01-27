dd26635d745368d70acdc8b0ad985055f53c7c14
michael_kahle <michael.kahle@ub.uni-freiburg.de>
2017-01-27 18:57:46 +0100

    Add some more plot types - first draft only

---
+++ R/drawValueBoxPlot.r
@@ -0,0 +1,33 @@
+source("R/tmbImportPackages.R")
+
+installDrawPackages <- function() {
+  # define needed packages
+  packages <- c("extrafont", "ggmap", "ggplot2")
+  tmbImportPackages(packages)
+}
+
+#' Draw summary of all tambora data to a map
+#'
+#' This function allows you to draw data inquired from tambora.org to a map
+#' @param tamboraData, i.e. as inquired by function fromTambora()
+#' @keywords tambora.org map visualisation
+#' @export
+#' @examples
+#' data<-fromTambora(); drawWorldMapAll(data)
+
+drawValueBoxPlot <- function(tamboraData) {
+  installDrawPackages()
+  
+  mean_index <- aggregate(data[,'si_average'], list(data$begin_year), mean)
+
+  #match to two columns with different length by machting criteria
+  data$mean <- mean_index[match(data$begin_year, mean_index$Group.1), 'x']
+
+  ggplot(aes(y = si_average, x = begin_year), data = data, x) 
+    +labs(fill= "Index", x = "Year", y = "Temperature Index", title = "")
+    +geom_boxplot(aes(fill=mean, group = cut_width(begin_year, 1)), outlier.alpha = 0.1)
+    +scale_y_continuous(breaks=c(-3,-2,-1,0,1,2,3))
+    +scale_fill_gradient2(low="blue", mid="green", high="red", limits=c(-3,+3))
+    +geom_jitter(size = 0.5,  width = 0.5, height = 0.25)
+    +geom_smooth()
+}
---
+++ R/drawYearHistogramm.R
@@ -0,0 +1,21 @@
+source("R/tmbImportPackages.R")
+
+installDrawPackages <- function() {
+  # define needed packages
+  packages <- c("extrafont", "ggplot2")
+  tmbImportPackages(packages)
+}
+
+#' Draw summary of all tambora data to a map
+#'
+#' This function allows you to draw data inquired from tambora.org to a map
+#' @param tamboraData, i.e. as inquired by function fromTambora()
+#' @keywords tambora.org map visualisation
+#' @export
+#' @examples
+#' data<-fromTambora(); drawWorldMapAll(data)
+
+drawYearHistogramm <- function(tamboraData) {
+  installDrawPackages()
+  ggplot(data, aes(begin_year)) + geom_histogram(color = "#6F9BC5", fill = "#6F9BC5", binwidth = 0.5)
+}
