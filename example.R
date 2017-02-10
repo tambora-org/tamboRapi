c4416c686207a7c4a2e2614399ede099fe035888
KMicha <michael.kahle@yahoo.de>
2017-02-10 18:10:08 +0100

    Add example for solar eclipses and histogram per year

--- example.R
+++ example.R
@@ -10,5 +10,10 @@ tamboRapi::drawWorldMapAll(data1)
 
 # inquire data with temperature level within timerange
 data2 <- tamboRapi::fromTambora("c[sc]=1&t[yb]=1800&t[ye]=1830")
-# draw a map of all events
+# draw a box plot for indices
 tamboRapi::drawIndexBoxPlot(data2)
+
+# inquire data for solar eclipses within europe
+data3 <- tamboRapi::fromTambora("c[nd]=315&s[lt1]=33.28&s[lg1]=-7.73&s[lt2]=61.10&s[lg2]=26.54")
+# draw a histogramm across the years
+tamboRapi::drawYearHistogramm(data3)
