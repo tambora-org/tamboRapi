726a17b6d5559f010974a4c27cc12b008d46347a
KMicha <michael.kahle@yahoo.de>
2017-02-10 18:02:20 +0100

    Add one more example

--- example.R
+++ example.R
@@ -4,7 +4,11 @@
 devtools::install_github('tambora-org/tamboRapi')
 
 # inquire data from collection 156: The Grotzfeld data set
-data <- tamboRapi::fromTambora("g[cid]=156")
-
+data1 <- tamboRapi::fromTambora("g[cid]=156")
 # draw a map of all events
-tamboRapi::drawWorldMapAll(data)
+tamboRapi::drawWorldMapAll(data1)
+
+# inquire data with temperature level within timerange
+data2 <- tamboRapi::fromTambora("c[sc]=1&t[yb]=1800&t[ye]=1830")
+# draw a map of all events
+tamboRapi::drawIndexBoxPlot(data2)
