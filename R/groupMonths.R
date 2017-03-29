18b56853a6b86a67cb7258ad62a7dbbb097ac6d0
rh1025 <rh1025@public.ads.uni-freiburg.de>
2017-03-29 16:39:44 +0200

    Add function for weightning months

--- R/groupMonths.R
+++ R/groupMonths.R
@@ -31,8 +31,49 @@ groupMonths <- function(tamboraData) {
   moy <- as.matrix(moy)
   # combine result and tamboraData to tamboraDataMonth
   tamboraDataMonth <- cbind(tamboraData, moy)
-  
   # return value
   tamboraDataMonth 
 }
-	
+
+#help function to calculate fraction of month
+fnc_fraction <- function(x) {
+  a <- as.numeric(x[["moy"]]);
+  b <- as.numeric(x[["b_month"]]);
+  e <- as.numeric(x[["e_month"]]);
+  r <- 1;
+  if (a==floor(b)) {
+    r <- 1+a-b;  
+  }
+  if (a==floor(e)) {
+    r <- e-a;  
+  }  
+  r; 
+}
+
+weightMonths <- function(tamboraData) {
+  installGroupPackages()
+  # define absolute time interval
+  b <- interval(ymd("0000-01-01"), tamboraData$begin_timestamp)
+  e <- interval(ymd("0000-01-01"), tamboraData$end_timestamp)
+  # convert time interval to months
+  b_month <- b / months(1)
+  e_month <- e / months(1)
+  # combine to begin and end to one list
+  l_months <- cbind(b_month, e_month)
+  # list of all covered months
+  month_list <- apply(l_months, 1, rangeOfMonths)
+  # define list of all coverd months of years as moy
+  moy <- month_list
+  # make list to matrix
+  moy <- as.matrix(moy)
+  # combine result and tamboraData to tamboraDataMonth
+  tamboraDataMonth <- cbind(tamboraData, b_month, e_month, moy)
+  # unnesting lists [according to months) to rows
+  unnest_data <- unnest(tamboraDataMonth, moy)
+  # calculate fraction respectively weighting for each month
+  unnest_data$time_weight <- apply(unnest_data, 1, fnc_fraction)
+  # get all covered months in year (range:1-12)
+  unnest_data$moy <- unnest_data$moy %% 12 + 1
+  # return value
+  unnest_data
+}
